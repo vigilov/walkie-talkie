101253b99eee8104cd43e56594fc52e3c2061f63
k.bokov <k.bokov@trafficstars.com>
2022-11-26 22:10:42 +0200

    User matching algorithm adjusted

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import logging
-
 import sentence_transformers
 import firebase_admin
 import firebase_admin.credentials
-import firebase_admin.firestore
+from firebase_admin import firestore
 import signal
+import operator
 
-cred_path = "walkie-talkie-limassol-firebase-adminsdk-hvxbi-663355e2dc.json"
+
+cred_path = "walkie-talkie-limassol-firebase.json"
 cred = firebase_admin.credentials.Certificate(cred_path)
 firebase_admin.initialize_app(cred)
-collection_chats = firebase_admin.firestore.client().collection('chats')
+db = firestore.client()
+
+collection_users = db.collection('users')
+collection_chats = db.collection('chats')
+collection_messages = db.collection('messages')
 logging.info(f"{cred_path} registered")
 
 sentence_model = sentence_transformers.SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
@@ -53,33 +58,50 @@ def on_snapshot(col_snapshot, changes, read_time):
     logging.info(f"on_snapshot(col_snapshot) called, len(col_snapshot) == {len(col_snapshot)}")
     batch = firebase_admin.firestore.client().batch()
 
+    precalculated_users = list()
     # Load all resolved chats
-    resolved_chats = collection_chats.where('status', '==', 'resolved').get()
-    # Calculate a embedding for each
-    precalculated_chats = []
-    for resolved_chat in resolved_chats:
-        first_message = resolved_chat.get('firstMessage')
-        first_message_tensor = sentence_model.encode(first_message, convert_to_tensor=True)
-        precalculated_chats.append((first_message_tensor, resolved_chat))
+    # resolved_chats = collection_chats.where('status', '==', 'resolved').get()
+    # for resolved_chat in resolved_chats:
+    #     first_message = resolved_chat.get('firstMessage')
+    #     first_message_tensor = sentence_model.encode(first_message, convert_to_tensor=True)
+    #     precalculated_users.append((first_message_tensor, resolved_chat))
+
+    business_users = collection_users.where('roles', 'array_contains', 'business').get()
+    for user in business_users:
+        bio = user.get("bio")
+        if bio != "":
+            bio_tensor = sentence_model.encode(bio, convert_to_tensor=True)
+            precalculated_users.append({"tensor": bio_tensor, "user": user.to_dict(), "multiplier": 1.2})
+
+    expert_users = collection_users.where('roles', 'array_contains', 'expert').get()
+    for user in expert_users:
+        bio = user.get("bio")
+        if bio != "":
+            bio_tensor = sentence_model.encode(bio, convert_to_tensor=True)
+            precalculated_users.append({"tensor": bio_tensor, "user": user.to_dict(), "multiplier": 1.1})
+
 
     for pending_chat in col_snapshot:
         first_message = pending_chat.get('firstMessage')
-        # Calculate a embedding for each chat with 1 member
+        # Calculate embedding for each chat with 1 member
         first_message_tensor = sentence_model.encode(first_message, convert_to_tensor=True)
         # Find the most relevant one
-        chats_by_relevance = sorted([(
-            sentence_transformers.util.pytorch_cos_sim(first_message_tensor, resolved_tensor),
-            resolved_chat
-        ) for resolved_tensor, resolved_chat in precalculated_chats],
-            key=lambda x: float(x[0][0][0])  # x be like pytorch.tensor([[0.42]])
-        )
 
+        rated_users = []
+        for user in precalculated_users:
+            rate = sentence_transformers.util.pytorch_cos_sim(first_message_tensor, user["tensor"])
+            rated_users.append({"user": user["user"], "rate": float(rate) * user["multiplier"]})
+
+        rated_users.sort(key=operator.itemgetter('rate'))
         # Set up the most relevant expert who has not yet tried to answer this question
-        for score, relevant_chat in chats_by_relevance:
-            if responder := relevant_chat.get("responser") in pending_chat.get("unMatchedParticipants"):
+        for user in rated_users:
+            print(user)
+            if responder := user["user"]["id"] in pending_chat.get("unMatchedParticipants"):
                 continue
-            batch.update(collection_chats.document(pending_chat.id), {'status': 'open', 'responder': responder})
-            logging.info(f"pending chat {pending_chat.id} matched with chat {relevant_chat}, score == {score}")
+
+            batch.update(collection_chats.document(pending_chat.id), {'status': 'opened', 'responser': user})
+            logging.info(f"pending chat {pending_chat.id}"
+                         f" matched with user {user['user']['id']}, score == {user['rate']}")
 
     batch.commit()
     logging.info("on_snapshot -> batch.commit(), new matches saved")
