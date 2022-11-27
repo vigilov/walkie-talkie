598dafae50bb430888d3c166c460185fbe2d55c6
i.vigilov <vigilov@traffictstars.com>
2022-11-27 17:54:10 +0200

    others chats

--- middleware/chat.ts
+++ middleware/chat.ts
@@ -2,18 +2,22 @@ import {navigateTo, useAuthUser, useChat} from "#imports";
 
 export default defineNuxtRouteMiddleware(async (to) => {
     if (process.client) {
-        const chatID = useState("chat_id", () => to.params.id)
-        chatID.value = to.params.id
+        try {
+            const chatID = useState("chat_id", () => to.params.id)
+            chatID.value = to.params.id
 
-        const authUser = await useAuthUser()
-        const chat = await useChat(<string>to.params.id)
+            const authUser = await useAuthUser()
+            const chat = await useChat(<string>to.params.id)
 
-        if (!authUser) {
-            navigateTo("/login")
-        }
+            if (!authUser) {
+                navigateTo("/login")
+            }
 
-        if (chat.createdBy != authUser?.uid && (!chat.responser || chat.responser.id != authUser?.uid)) {
-            navigateTo("/chats")
+            if (chat.createdBy != authUser?.uid && (!chat.responser || chat.responser.id != authUser?.uid)) {
+                navigateTo("/chats")
+            }
+        } catch (e) {
+            console.log("middleware error", e)
         }
     }
 })
